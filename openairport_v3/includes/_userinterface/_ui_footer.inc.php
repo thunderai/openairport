7def51d0d3693353b54ac959a053a80c25470b74
Erick.Dahl@gmail.com <Erick.Dahl@gmail.com@9787e1be-f656-f5a4-7c1b-7a5d3974c427>
2012-10-14 16:57:01 +0000

    Work on the breadcrum trail for Part 327 report new

--- openairport_v3/includes/_userinterface/_ui_footer.inc.php
+++ openairport_v3/includes/_userinterface/_ui_footer.inc.php
@@ -131,14 +131,19 @@ eval(temp)
 </script>
 <?php
 $time_init 				= $_SESSION['page_time'];
-$_SESSION['page_time'] 	= microtime();
+$_SESSION['page_time'] 	= microtime(true);
 $time_completed 		= $_SESSION['page_time'];
 
 //echo "Page Ended at time index [".$time_completed."] <br>";
 $time_taken	= ($time_completed - $time_init);
 //$time_taken	= ($time_taken * 1000);
 $time_taken	= round($time_taken,6);
-echo "This page took ".$time_taken." micro seconds to load";
+//echo "This page took ".$time_taken." micro seconds to load";
+
+$message = "This page has taken ".$time_taken." microseconds to load";
 ?>
+	<script>
+		parent.document.getElementById("timetaken").innerHTML = "<?php echo $message;?>";
+		</script>
 		</body>
 	</html>
